389b3b24a7dcd89b51fd991f42fdb6858171c45b
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-11-04 12:02:25 +0000

    Add files via upload

--- sound_recog/client.py
+++ sound_recog/client.py
@@ -1,8 +1,20 @@
 import requests
 from flask import Flask,request,jsonify
 URL="http://127.0.0.1:5000//predict"# The link provided, running in the local host
-TEST_AUDIO_FILE_PATH="D://sound_recog//music//bhairavi//Bhairavi01.wav"
+import sounddevice
+from scipy.io.wavfile import write
+fs= 44100
+second =  int(input("Enter time duration in seconds: "))
+# Temporary option
+print("Recording.....n")
+record_voice = sounddevice.rec( int ( second * fs ) , samplerate = fs , channels = 2 )
+sounddevice.wait()
+write("out.wav",fs,record_voice)
+print("Finished.....Please check your output file")
 
+
+#TEST_AUDIO_FILE_PATH="D://sound_recog//music//bhairavi//Bhairavi01.wav"
+TEST_AUDIO_FILE_PATH="C:\\Users\\Biancaa. R\\flask app\\out.wav"
 if __name__=="__main__":
     audio_file=open(TEST_AUDIO_FILE_PATH,"rb")
     values={"file":(TEST_AUDIO_FILE_PATH,audio_file,"audio/wav")}
